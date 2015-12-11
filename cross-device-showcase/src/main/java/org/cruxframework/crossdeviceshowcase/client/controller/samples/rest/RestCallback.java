4becf8c780a70d782b0d814512f492ead1d3b1f4
Samuel Cardoso <samuel.cardoso@triggolabs.com>
2015-12-11 16:29:06 -0200

    Issue #2 - Make Showcase project compatible with Crux 5.4

--- cross-device-showcase/src/main/java/org/cruxframework/crossdeviceshowcase/client/controller/samples/rest/RestCallback.java
+++ cross-device-showcase/src/main/java/org/cruxframework/crossdeviceshowcase/client/controller/samples/rest/RestCallback.java
@@ -4,6 +4,8 @@ import org.cruxframework.crux.core.client.rest.Callback;
 import org.cruxframework.crux.smartfaces.client.dialog.MessageBox;
 import org.cruxframework.crux.smartfaces.client.dialog.MessageBox.MessageType;
 import org.cruxframework.crux.smartfaces.client.dialog.WaitBox;
+import org.cruxframework.crux.smartfaces.client.label.Label;
+import org.cruxframework.showcasecore.client.resource.ShowcaseCoreMessages;
 
 import com.google.gwt.core.shared.GWT;
 
@@ -11,6 +13,7 @@ public abstract class RestCallback<T> implements Callback<T>
 {
 	private final static WaitBox wait = new WaitBox();
 	private static RestMessages messages = GWT.create(RestMessages.class);
+	private static ShowcaseCoreMessages coreMessages = GWT.create(ShowcaseCoreMessages.class);
 	
 	private void closeWaitPopup()
 	{
@@ -23,6 +26,7 @@ public abstract class RestCallback<T> implements Callback<T>
 	public RestCallback()
 	{
 		wait.center();
+		wait.setWidget(new Label(coreMessages.loading()));
 	}
 
 	public abstract void onComplete(T result);
